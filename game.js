abab4ee514616d086b320ce5828bf16f0635541a
khobster <152924497+khobster@users.noreply.github.com>
2024-07-01 20:43:08 -0400

    Update game.js

--- game.js
+++ game.js
@@ -55,7 +55,7 @@ function create() {
     });
 
     // Add the batter and pitcher sprites with correct positions and scaling
-    batter = this.physics.add.sprite(350, 3335, 'batter').setScale(1.8).setOrigin(0.5, 1);
+    batter = this.physics.add.sprite(350, 335, 'batter').setScale(1.8).setOrigin(0.5, 1);
     pitcher = this.physics.add.sprite(400, 308, 'pitcher').setScale(1.8).setOrigin(0.5, 1);
     ball = this.physics.add.sprite(400, 500, 'ball').setScale(1.5).setOrigin(0.5, 0.5);
 
